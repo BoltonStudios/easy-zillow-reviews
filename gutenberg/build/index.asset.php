bf752cc00f15a9513cb5de64a265b83e2f06a0be
Aaron Bolton <aaron@boltonstudios.com>
2023-03-21 03:52:56 -0400

    Ran npm build command to rebuild gutenberg assets

--- gutenberg/build/index.asset.php
+++ gutenberg/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-blocks', 'wp-components', 'wp-compose', 'wp-element', 'wp-i18n'), 'version' => 'dca16958df72a855d95f');
+<?php return array('dependencies' => array('wp-blocks', 'wp-components', 'wp-compose', 'wp-element', 'wp-i18n'), 'version' => '417c6e4c073a59ca104d');
